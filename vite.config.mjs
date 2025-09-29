4140a307136bc90c3ccef277546be24a65cb5b15
Robbi <zen33625@gmail.com>
2025-09-29 17:48:23 +0200

    build fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,6 @@
 import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
-import viteImagemin from 'vite-plugin-imagemin'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -19,24 +18,5 @@ export default defineConfig({
     },
   },
 
-  plugins: [
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
 
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 20,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-
-    }),
-  ],
 })
