c344d93a4846f30269c19f6f7bb62c97f915e43a
Robbi <zen33625@gmail.com>
2025-10-03 12:14:28 +0200

    Fix links

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,28 +1,14 @@
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: './',
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        // catalog: resolve(__dirname, 'catalog.html'),
-        // notes: resolve(__dirname, 'notes.html'),
-        // about: resolve(__dirname, 'about.html'),
-
-      },
       output: {
-        // файли завжди без хеша в назві
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
         assetFileNames: `assets/[name].[ext]`
       }
-    },
-  },
-
-
+    }
+  }
 })
